f87cabc45be91614543f7fc6cb8311e7ef06728f
pornoiya <49945883+pornoiya@users.noreply.github.com>
2023-04-07 18:07:44 +0500

    Update solution.py
    upd solution

--- solution.py
+++ solution.py
@@ -7,11 +7,10 @@ from scipy.stats import norm
 chat_id = 146155552 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
+    t = 77
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    min = (-x).min()
+   
+    z_2 = -np.log(1-p)/x.size    
+    
+    return 2*(-min-1/2)/(t*t), 2*(z_2-min-1/2)/(t*t)
